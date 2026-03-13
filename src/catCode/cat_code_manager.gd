f75591cfb213bb0a22d2fa0605d4b20e4880deb4
Drewan Rutherford <DrewanR@protonmail.com>
2026-03-13 00:45:11 +0000

    Threading (and potentially multi-threading) added

--- src/catCode/cat_code_manager.gd
+++ src/catCode/cat_code_manager.gd
@@ -1,4 +1,7 @@
-extends Node
+@tool
+class_name code_manager extends Node
+
+@export_group("Relationships")
 
 ## Parent node that this will control
 @export var parent :Node
@@ -8,6 +11,8 @@ extends Node
 
 ## Node to output any text to.
 @export var text_output :Node
+
+@export_subgroup("Optional:")
 
 ## Source of instruction nodes. [br]
 ## NOTE: If left blank, this will default to self
@@ -21,21 +26,36 @@ extends Node
 ## NOTE: If left blank, this will default to the [text_output] node
 @export var error_output :Node = text_output
 
+@export_group("Aesthetics")
+
 ## Show instruction set on boot
 @export var show_instruction_set := true
 
-## Autorun states
+@export_group("Execution options")
+
+## Auto-run states
 enum RUN_OPTIONS { NEVER, READY, PROCESS, PRIMARY_ACTION, SECONDARY_ACTION}
 @export var when_to_run: RUN_OPTIONS
 
-## Compile options,
-## Default will compile 
+## Compile options, [br]
+## NEVER: Will never compile, use if another node handles compilation.
+## RUN: Will compile when.
+## DEFAULT: Will infer behavior from auto-run. [br]
+## 
+## WARNING: Compiling will kill all the current threads
 enum COMPILE_OPTIONS { NEVER, RUN, DEFAULT}
-@export var when_to_compile: COMPILE_OPTIONS = COMPILE_OPTIONS.DEFAULT
+@export var when_to_compile :COMPILE_OPTIONS = COMPILE_OPTIONS.DEFAULT
 
-## The maximum number of threads that can exist symultaneously. [br]
+## The maximum number of threads that can exist simultaneously. [br]
 ## WARNING: Multiple threads are untested.
 @export var maximum_threads :int = 1
+
+## Thread overflow behavior.
+## NEVER_REPLACE: Never override the existing threads, will disregard the new process.
+## ALWAYS_REPLACE: Will insert the new thread, killing the lowest priority (this may
+##                 potentially kill the new process before it starts).
+enum THREAD_OVERFLOW_BEHAVIORS { NEVER_REPLACE, ALWAYS_REPLACE}
+@export var thread_overflow_behavior := THREAD_OVERFLOW_BEHAVIORS.NEVER_REPLACE
 
 ## List containing logicBlocks. [br]
 ## These are the backend instructions containing within [res://src/catCode/logicBlocks/]
@@ -43,26 +63,23 @@ enum COMPILE_OPTIONS { NEVER, RUN, DEFAULT}
 ## These are taken from the children of this class. [br][br]
 ## [br]View [docs/catCode.md] for more information.
 var instruction_list :Array[logic_block] = []
+
 ## Dictionary containing the same information.
 var instruction_dict :Dictionary[String, logic_block] = {}
 # Both were used as please cats have mercy, this is already hell to deal with.
 
 ## Compiled code. [br]
-## Note: This function should never compile code
+## Note: This class should never compile code
 var compiled_code :Array[instruction_line] = []
 
 ## Array for storing the thread instances,
-## These threads are used to seperate out the running of code from the manager.
+## These threads are used to separate out the running of code from the manager.
 ## Typically, only one thread will exist at once. All threads are destroyed when
 ##    code is recompiled. 
+var threads :Array[cat_thread] = []
 
-## Enum for the states of how a code will be treated: [br]
-## DO: will result in the code being executed,
-## SKIP_1: will skip until the next control statement of proper indent,s
-## SKIP_ALL: will skip to the end of the indent.
-enum PASS_STATES { DO, SKIP, SKIP_ALL}
-## Enum for the type of layer in the indentStack
-enum STACK_LAYER_TYPE { NONE, CONDITIONAL, ITERATIVE }
+## Preload the cat_thread node
+var cat_thread_node = preload("res://src/catCode/cat_thread.tscn")
 
 ## Signal emitted upon updating the instructions
 signal instructions_updated(new_list :Array[logic_block], new_dict :Dictionary[String, logic_block])
@@ -84,7 +101,7 @@ func _ready() -> void:
 	
 	match when_to_run:
 		RUN_OPTIONS.READY:
-			run()
+			run_script()
 		RUN_OPTIONS.PRIMARY_ACTION:
 			print_line("Press [Z] to run!")
 		RUN_OPTIONS.SECONDARY_ACTION:
@@ -93,163 +110,66 @@ func _ready() -> void:
 	editor.compile()
 
 func _process(_delta: float) -> void:
-	if when_to_run == RUN_OPTIONS.PROCESS:
-		run()
-	elif when_to_run == RUN_OPTIONS.SECONDARY_ACTION and Input.is_action_just_pressed("secondary_action"):
-		run()
-	elif when_to_run == RUN_OPTIONS.PRIMARY_ACTION and Input.is_action_just_pressed("primary_action"):
-		run()
+	clean_threads()
 
-func run():
+	if when_to_run == RUN_OPTIONS.PROCESS:
+		run_script()
+	elif when_to_run == RUN_OPTIONS.SECONDARY_ACTION and Input.is_action_just_pressed("secondary_action"):
+		run_script()
+	elif when_to_run == RUN_OPTIONS.PRIMARY_ACTION and Input.is_action_just_pressed("primary_action"):
+		run_script()
+
+## Spawns and executes a thread, if applicable.
+func run_script():
 	if when_to_run != RUN_OPTIONS.PROCESS: print_line("Running...")
 	if when_to_compile == COMPILE_OPTIONS.RUN: editor.compile()
 	#editor.compile()
 	
-	## Indent stack, used to control indents and skipping, 
-	var indent_stack := [stateStackLayer.new(0, STACK_LAYER_TYPE.NONE, PASS_STATES.DO, 0)]
-	# Iterates across every line
-	#for line in compiled_code:
-	var line_number = 0
-	print("Running...")
-	while line_number < len(compiled_code):
-		var line = compiled_code[line_number]
-		print("--Current Stack: " + str(indent_stack))
-		print("  " + str(line))
-		
-		# Functions: ALL
-		if (
-			line.is_executable_function() and # The current line is executable
-			indent_stack.back().state == PASS_STATES.DO and # The stack is currently in a "do" state
-			indent_stack.back().indent == line.indent # The indents match
-			):
-			#print("! Running " + line.primary_block.block_name)
-			line_number += 1
-			if line.valid_parameters() and line.uses_parameters():
-				line.primary_block.execute(line.parameters)
-			elif line.valid_parameters():
-				line.primary_block.execute()
-			else:
-				print_line("> /!\\ Invalid parameter count...")
-		
-		# Conditionals: IF
-		elif (
-			line.is_selective_element() and # Is a selective block
-			line.primary_block.block_ref == "if" and # And is an if statement
-			indent_stack.back().state == PASS_STATES.DO and # The stack is currently in a "do" state
-			indent_stack.back().indent == line.indent # The indents match
-			):
-			#print("! Reached an if statement of indent " + str(line.indent))
-			line_number += 1
-			if line.primary_block.evaluate(line.parameters):
-				#print("  True")
-				indent_stack.push_back(
-					stateStackLayer.new(line.indent+1, STACK_LAYER_TYPE.CONDITIONAL, PASS_STATES.DO, line_number)
-				)
-			else:
-				#print("  False")
-				indent_stack.push_back(
-					stateStackLayer.new(line.indent+1, STACK_LAYER_TYPE.CONDITIONAL, PASS_STATES.SKIP, line_number)
-				)
-		
-		# Conditionals: ELIF TODO: Test
-		elif (
-			line.is_selective_element() and # Is a selective block
-			line.primary_block.block_ref == "elif" and # And is an else statement
-			len(indent_stack) > 1 and # Indent stack is large enough for an else state to be possible
-			indent_stack[-2].state == PASS_STATES.DO and # The stack is currently in a "do" state
-			indent_stack[-2].indent == line.indent # The indents match
-		):
-			#print("! Reached an else if statement of indent " + str(line.indent))
-			line_number += 1
-			indent_stack.back().acc += 1
-			# Scenario 1: Currently in "SKIP" state
-			if indent_stack.back().state == PASS_STATES.SKIP:
-				indent_stack.back().state = PASS_STATES.DO if line.primary_block.evaluate(line.parameters) else PASS_STATES.SKIP
-			# Scenario 2: Currently in "DO" state
-			else:
-				indent_stack.back().state = PASS_STATES.SKIP_ALL
-		
-		# Conditionals: ELSE
-		elif (
-			line.is_selective_element() and # Is a selective block
-			line.primary_block.block_ref == "else" and # And is an else statement
-			(
-				len(indent_stack) > 1 and # Indent stack is large enough for an else state to be possible
-				indent_stack[-2].state == PASS_STATES.DO and # The stack is currently in a "do" state
-				indent_stack[-2].indent == line.indent # The indents match
-			)
-		):
-			#print("! Reached an else statement of indent " + str(line.indent))
-			line_number += 1
-			indent_stack.back().acc += 1
-			indent_stack.back().state = PASS_STATES.DO if indent_stack.back().state == PASS_STATES.SKIP else PASS_STATES.SKIP_ALL
-		
-		# Iterables: REPEAT (initiation)
-		elif (
-			line.is_iterative_element() and # Is an iterative block
-			line.primary_block.block_ref in ["repeat", "repeatForever"] and # And is a repeat statement
-			indent_stack.back().state == PASS_STATES.DO and # The stack is currently in a "do" state
-			indent_stack.back().indent == line.indent # The indents match
-		):
-			#print("! Reached a repeat statement of indent " + str(line.indent))
-			# Do not incrment line
-			indent_stack.push_back(
-				stateStackLayer.new(line.indent+1, STACK_LAYER_TYPE.ITERATIVE, PASS_STATES.DO, line_number)
-			)
-			#indent_stack.back().entry = line_number - 1
-		
-		# Iterables: REPEAT (evaluation)
-		elif (
-			line.is_iterative_element() and # Is an iterative block
-			line.primary_block.block_ref in ["repeat", "repeatForever"] and # And is a repeat statement
-			len(indent_stack) > 1 and # Indent stack is large enough for this state to be possible
-			indent_stack[-2].state == PASS_STATES.DO and # The stack is currently in a "do" state
-			indent_stack[-2].indent == line.indent # The indents match
-		):
-			print("! Re-reached a repeat statement of indent " + str(line.indent))
-			line_number += 1
-			indent_stack.back().rep += 1 # Increment accumulator
-			if line.primary_block.evaluate([line.parameters[0], indent_stack.back()]):
-				print("  Loop continuing...")
-				indent_stack.back().state = PASS_STATES.DO
-			else:
-				print("  Loop ending...")
-				indent_stack.back().state = PASS_STATES.SKIP_ALL
-			
-			if line.primary_block.should_wait_for_next_frame(): # Waits for next frame if instructed to do so
-				await get_tree().process_frame
-		
-		# Iterables: ENDLOOP
-		elif (
-			indent_stack.back().type == STACK_LAYER_TYPE.ITERATIVE and # Is in the iterative block.
-			line.indent + 1 == indent_stack.back().indent # Indent is 1 less than before.
-		):
-			print("! End of loop section reached")
-			
-			if indent_stack.back().state == PASS_STATES.DO: # If in do state, continues within the loop
-				print("    Jumping back to start")
-				line_number = indent_stack.back().entry
-			else: # Else: ends the loop
-				print("    Proceding onwards")
-				indent_stack.pop_back()
-		
-		# Skip conditions
-		elif (
-			indent_stack.back().state in [PASS_STATES.SKIP, PASS_STATES.SKIP_ALL] and
-			indent_stack.back().indent == line.indent
-		):
-			line_number += 1
-			print("  Skipping " + line.primary_block.block_name)
-		
-		# End indent block, pops the current stack layer
-		elif len(indent_stack) > 1 and indent_stack.back().type == STACK_LAYER_TYPE.CONDITIONAL:
-			print("  Popping...")
-			indent_stack.pop_back()
-		
-		# And if none of the above is true, the line is skipped
+
+	# Case 1: below maximum thread count
+	if (len(threads) < maximum_threads):
+		create_thread()
+	# Case 2: 
+	elif (
+		len(threads) == maximum_threads and 
+		thread_overflow_behavior == THREAD_OVERFLOW_BEHAVIORS.ALWAYS_REPLACE
+	):
+		create_thread()
+
+
+## Removes and queue_free()s all the dead threads
+func clean_threads():
+	var index = 0
+	while index < len(threads):
+		if threads[index].thread_state == cat_thread.THREAD_STATES.DEAD:
+			kill_thread(index)
 		else:
-			print("  Advancing...")
-			line_number += 1
+			index += 1
+
+
+## Create Thread
+func create_thread():
+	# Creates the new thread
+	var new_thread = cat_thread_node.instantiate()
+
+	# Inserts it at the appropriate point
+	for i in range(0,len(threads)):
+		if threads[i].priority <= new_thread.priority:
+			threads.insert(i, new_thread)
+			return
+	threads.append(new_thread)
+
+	# Adds child
+	add_child(new_thread)
+
+
+## Kills a thread in the thread array at [param index].
+## This will both remove the array item and queue the thread free.
+func kill_thread(index :int):
+	var thread = threads.pop_at(index)
+	thread.thread_state = thread.THREAD_STATES.KILLED
+	thread.queue_free()
+
 
 ## Updates the blocks in both the instruction dictionary and list
 func update_instructions(source := self) -> void:
@@ -311,20 +231,3 @@ func print_instruction_dictionary() -> void:
 func _on_compiled_code_received(new_code):
 	compiled_code = new_code
 
-
-class stateStackLayer:
-	var indent :int
-	var type :STACK_LAYER_TYPE
-	var state :PASS_STATES
-	var rep :int
-	var entry :int
-	
-	func _init(_indent :int, _type :STACK_LAYER_TYPE, entry_state :PASS_STATES, _entry :int) -> void:
-		indent = _indent
-		type = _type
-		state = entry_state
-		entry = _entry
-		rep = 0
-	
-	func _to_string() -> String:
-		return "<" + str(indent) + ": " + STACK_LAYER_TYPE.keys()[type][0] + " state:" + str(state) + " rep:" + str(rep) + " entry:" + str(entry) + ">"
